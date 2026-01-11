31810e543fec6a6ee9dabd39f664848d37e4788e
Selim Efe <uysalselimefe@gmail.com>
2026-01-11 13:25:52 +0300

    Implement reverse search logic with constraint filtering
    Added source code that iterates downwards from 100 to populate a buffer with valid integers, using Last-In-First-Out (LIFO) printing logic to achieve ascending order output.

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+                  int i,array[10],index,j;
+
+    index=0;
+
+    for(i=99;;i--){
+        if(i%2==0){
+            if(i%3!=0){
+                array[index]=i;
+                index++;
+            }
+        }
+        if(index==10){
+            break;
+        }
+    }
+    for(j=9;j>=0;j--){
+        printf("%d ",array[j]);
+    }
+
+
+    return 0;
+}
